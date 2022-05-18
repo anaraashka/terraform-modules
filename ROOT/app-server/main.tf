5a0939a3d80438769198e170d5813abd0cfd8d0f
anaraashka <anaraashka@yahoo.com>
2022-05-18 17:52:34 -0500

    added public subnet output

--- ROOT/app-server/main.tf
+++ ROOT/app-server/main.tf
@@ -1,11 +1,11 @@
-module "vpc" {
-  source = "../../CHILD/VPC"
-}
-
 module "app-vpc" {
   source = "../../CHILD/VPC"
   vpc_cidr = "10.91.0.0/16"
   pub_cidr = "10.91.1.0/24"
   prv_cidr = "10.91.2.0/24"
   tag = "App"
+}
+module "app-server" {
+  source = "git::https://github.com/anaraashka/terraform-modules.git//CHILD/EC2"
+  # source = "git@github.com:anaraashka/terraform-modules/tree/main/CHILD/EC2"
 }
